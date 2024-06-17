28e051ee16f58c509bbfe3a57f4a4c0b0665344b
yves hoebeke <yves.hoebeke@bytesupply.com>
2024-06-17 16:37:12 -0400

    better mem alloc and delimiters detection

--- strexplode.h
+++ strexplode.h
@@ -5,49 +5,68 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAX_BUFFER_WIDTH 100
+#define DELIMITERS_ALLOWED 2
 
-char** strexplode(char* string, char delimeter, int* count) {
+char** strexplode(char* string, char delimiter, int* count) {
     char** result;
-    char buffer[MAX_BUFFER_WIDTH];
-    int word_count;
-    int word_length;
-    int result_idx = 0;
-    int buffer_idx = 0;
+    char* buffer;
+    char delimiters[DELIMITERS_ALLOWED] = {delimiter, '\0'};
+    const unsigned int subject_length = strlen(string);
+    unsigned int word_count = 0, result_idx = 0, buffer_idx = 0;
 
-    // get word count
-    for(int i = 0; i < strlen(string); i++){
-        if(string[i] == delimeter) {
-            word_count += 1;
-        }
-    }
-    *count = word_count;
-
-    // allocate mem for result
-    result = (char**)malloc((word_count + 1) * sizeof(char*));
-    if (result == NULL) {
-        fprintf(stderr, "Memory allocation failed\n");
+    // set subject string length bytes aside for work buffer (to be freed later)
+    buffer = (char*)malloc(subject_length * sizeof(char));
+    if (buffer == NULL) {
+        fprintf(stderr, "Buffer - Memory allocation failed\n");
+        *count = -1;
         return result;
     }
 
-    // allocate mem for each word and allocate value
-    for(int i = 0; i < strlen(string) + 1; i++){
-        if(string[i] != delimeter && string[i] != 0) {
-            buffer[buffer_idx++] = string[i];
-            word_length++;
-        } else {
-            buffer[buffer_idx] = 0;
-            result[result_idx] = (char*)malloc((word_length + 1) * sizeof(char));
-            if(result[result_idx] == NULL){
-                fprintf(stderr, "Memory allocation failed\n");
-                return result;
+    // get word count
+    for(int i = 0; i < strlen(string); i++){
+        if(string[i] == delimiters[0] || string[i] == delimiters[1]) {
+            if(i > 0 && string[i-1] != delimiters[0]){
+                word_count += 1;
             }
-            strcpy(result[result_idx], buffer);
-            result_idx++;
-            buffer_idx = 0;
-            word_length = 0;
         }
     }
+
+    // set return count [see issues]
+    *count = word_count + 1;
+
+    // allocate mem for result array for number of elements (words) found.
+    result = (char**)malloc((word_count + 1) * sizeof(char*));
+    if (result == NULL) {
+        fprintf(stderr, "Result array - Memory allocation failed\n");
+        free(buffer);
+        *count = -1;
+        return result;
+    }
+
+    // allocate mem for each word element (and commit value) to result array.
+    for(int i = 0; i < strlen(string) + 1; i++){
+        if(string[i] != delimiters[0] && string[i] != delimiters[1]) {
+            buffer[buffer_idx++] = string[i];
+        } else {
+            buffer[buffer_idx] = 0;
+            // skip if nothing in buffer, occurs when sequential delimiters are present.
+            if(strlen(buffer) > 0){
+                result[result_idx] = (char*)malloc(strlen(buffer) * sizeof(char));
+                if(result[result_idx] == NULL){
+                    fprintf(stderr, "Result array element - Memory allocation failed\n");
+                    free(buffer);
+                    *count = -1;
+                    return result;
+                }
+                strcpy(result[result_idx], buffer);
+                result_idx++;
+            }
+
+            buffer_idx = 0;
+        }
+    }
+
+    free(buffer);
 
     return result;
 }
--- strexplode_test.c
+++ strexplode_test.c
@@ -3,19 +3,19 @@
 
 int main() {
     // parameters for the function
-    char* instring = "now the time has come for all good men to come to the aid of their country";
-    char delimeter = ' ';
+    char* instring = "  now the time has come for all   good   men to come to the aid of their country";
+    char delimiter = ' ';
     int word_count;
     // show it to the people
-    printf("Given:\nSubject string: `%s`\nDelimeter: `%c`\n\nResult:\n\n", instring, delimeter);
+    printf("Given:\nSubject string: `%s`\n(note: sequential delimiters)\ndelimiter: `%c`\n\nResult:\n\n", instring, delimiter);
     // do it
-    char** result = strexplode(instring, delimeter, &word_count);
-    // show the labor
+    char** result = strexplode(instring, delimiter, &word_count);
+    // show the result
     printf("word count: %d\n", word_count);
     for(int i=0; i < word_count; i++){
         printf("[%d] `%s`\n", i, result[i]);
     }
-    // thst's it
+    // that's it
     printf("\ndone\n\n");
 
     return 0;
